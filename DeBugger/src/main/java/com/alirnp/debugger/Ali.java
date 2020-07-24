fd94a3d70770cfcdcdab595fca86dfecc1005ca3
ali <alimoghadam75@gmail.com>
2020-07-24 23:40:08 +0430

    add

---
+++ DeBugger/src/main/java/com/alirnp/debugger/Ali.java
@@ -0,0 +1,11 @@
+package com.alirnp.debugger;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Ali {
+
+    public static void show(Context context){
+        Toast.makeText(context, "Toast to show", Toast.LENGTH_SHORT).show();
+    }
+}
--- app/src/main/java/com/alirnp/debugutils/MainActivity.java
+++ app/src/main/java/com/alirnp/debugutils/MainActivity.java
@@ -3,6 +3,10 @@ package com.alirnp.debugutils;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Debug;
+import android.util.DebugUtils;
+
+import com.alirnp.debugger.Ali;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Ali.show(this);
     }
 }
